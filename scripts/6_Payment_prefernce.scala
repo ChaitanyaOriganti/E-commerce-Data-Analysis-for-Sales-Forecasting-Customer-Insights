69f9807acb7332354d996a3303fdc01e224a0504
Chaitanya Anudeep Origanti <30841887+anudeep1231@users.noreply.github.com>
2023-12-28 11:38:28 -0500

    Payment_prefernce.scala

--- scripts/6_Payment_prefernce.scala
+++ scripts/6_Payment_prefernce.scala
@@ -1,7 +1,7 @@
 // Payment prefernce:
 // ------------------
 
-:load 1_load_data.scala
+:load load_data.scala
 
 val ePairRdd7 = ePairRdd.map{ case(oid,cid,qt,cp,sp,ts,rating,pCat,pId,sp_type,
        oStat,pWt,pLen,pHt,pWidth,cCity,
