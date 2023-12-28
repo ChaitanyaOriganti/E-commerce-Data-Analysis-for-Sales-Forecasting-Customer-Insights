8050f966a960a34ba63faab0823a283c4c97c76f
Chaitanya Anudeep Origanti <30841887+anudeep1231@users.noreply.github.com>
2023-12-28 11:35:36 -0500

    Update and rename scripts/4_Hourly_sales_analysis.scala to Hourly_sales_analysis.scala

--- Hourly_sales_analysis.scala
+++ Hourly_sales_analysis.scala
@@ -1,7 +1,7 @@
 // Hourly sales analysis:
 // ----------------------
 
-:load 1_load_data.scala
+:load load_data.scala
 
 val TRdd=ePairRdd.map{case(oid,cid,qt,cp,sp,ts,rating,pCat,pId,sp_type,oStat,pWt,pLen,pHt,pWidth,cCity,cState,sId,sCity,sState,sInstal)=>
       val t1=ts.split(" ")(1).split(":")(0)
