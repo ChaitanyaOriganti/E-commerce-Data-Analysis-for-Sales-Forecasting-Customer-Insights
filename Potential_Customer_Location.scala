9d4e232b20aad7d9eb2243ce9ea299c00d8e7e36
Chaitanya Anudeep Origanti <30841887+anudeep1231@users.noreply.github.com>
2023-12-28 11:39:12 -0500

    Potential_Customer_Location.scala

--- Potential_Customer_Location.scala
+++ Potential_Customer_Location.scala
@@ -1,7 +1,7 @@
 // Potential Customer Location: - Statewise
 // ----------------------------
 
-:load 1_load_data.scala
+:load load_data.scala
 
 val ePairRdd13 = ePairRdd.map{ case(oid,cid,qt,cp,sp,ts,rating,pCat,pId,sp_type,
        oStat,pWt,pLen,pHt,pWidth,cCity,
