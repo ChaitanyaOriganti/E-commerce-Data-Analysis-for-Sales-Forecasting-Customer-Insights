e28d1318da9b6d68e1f813a5a9fee772bbc0d1f6
Chaitanya Anudeep Origanti <30841887+anudeep1231@users.noreply.github.com>
2023-12-28 11:38:08 -0500

    Monthly_Trend_Forecasting.scala

--- Monthly_Trend_Forecasting.scala
+++ Monthly_Trend_Forecasting.scala
@@ -1,7 +1,7 @@
 // Monthly Trend Forecasting:
 // -------------------------
 
-:load 1_load_data.scala
+:load load_data.scala
 
 val ePairRdd5 = ePairRdd.map{ case(oid,cid,qt,cp,sp,ts,rating,pCat,pId,sp_type,oStat,pWt,pLen,pHt,pWidth,cCity,cState,sId,sCity,sState,sInstal) =>
       val ds_ts = ts.split(" ")
